34036f551f65b85b1123e21d279eff47cb9bfed8
liangchengcheng <1038127753@qq.com>
2020-03-05 08:38:25 +0800

    初步调试——开始联调2eureka

--- web-service-eureka/src/main/java/com/hdsx/webservice/eureka/EurekaServiceApplication.java
+++ web-service-eureka/src/main/java/com/hdsx/webservice/eureka/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.hdsx.webservice;
+package com.hdsx.webservice.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
